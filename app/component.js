58684effcc6a7acd99cd5a8f1ef58292debe4bd7
Mark Chandler <mark.allen.chandler@gmail.com>
2017-04-07 23:20:12 -0400

    Import purecss in app

--- app/component.js
+++ app/component.js
@@ -1,10 +1,10 @@
-import styles from './main.css'
+import purecss from 'purecss'
 
 export default (text = 'Hello world') => {
   const element = document.createElement('div')
 
   element.innerHTML = text
-  element.className = styles.redButton
+  element.className = purecss['pure-button']
 
   return element
 }
